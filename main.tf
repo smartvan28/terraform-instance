f5dfe3fd3806cc67334dbde183ef2ca45ebfa0de
smartvan28 <65109750+smartvan28@users.noreply.github.com>
2022-10-28 08:49:50 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,6 @@
 terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-
-
-  required_version = ">= 0.13"
-}
-
-
-
-
-
-provider "yandex" {
-  token     = var.key1
-  cloud_id  = var.cloud_id1
-  folder_id = var.folder_id1
-  zone      = "ru-central1-a"
-}
-
-
+  
+  
 resource "yandex_compute_instance" "test" {
   name        = "test"
   platform_id = "standard-v1"
