8bb29aac2d9b7d6bf04560182ce1be774983ed66
smartvan28 <65109750+smartvan28@users.noreply.github.com>
2022-12-27 17:18:00 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -92,6 +92,15 @@ resource "yandex_lb_network_load_balancer" "foo1" {
       ip_version = "ipv4"
     }
   }
+  
+    listener {
+    name = "blackbox-port"
+    port = 9115
+    target_port = 9115
+     external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
 
   attached_target_group {
     target_group_id = "${yandex_lb_target_group.foo.id}"
